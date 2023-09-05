4c439e7cabfa1043889c2f7e5a049df72077df35
Hieu Do <vanhieu99it@gmail.com>
2023-09-05 14:44:08 +0700

    up DAO

--- src/main/java/com/hido/springwebapplication/dao/cartDao.java
+++ src/main/java/com/hido/springwebapplication/dao/cartDao.java
@@ -1,4 +1,35 @@
 package com.hido.springwebapplication.dao;
 
+import com.hido.springwebapplication.models.Cart;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Repository
 public class cartDao {
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf){ this.sessionFactory =sf;}
+
+    @Transactional
+    public Cart addCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().save(cart);
+        return cart;
+    }
+
+    @Transactional
+    public List<Cart> getCarts() {
+        return this.sessionFactory.getCurrentSession().createQuery("from CART").list();
+    }
+
+    @Transactional
+    public void updateCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().update(cart);
+    }
+
+    @Transactional
+    public void deleteCart(Cart cart) {
+        this.sessionFactory.getCurrentSession().delete(cart);
+    }
 }
--- src/main/java/com/hido/springwebapplication/dao/productDao.java
+++ src/main/java/com/hido/springwebapplication/dao/productDao.java
@@ -1,4 +1,52 @@
 package com.hido.springwebapplication.dao;
 
+import com.hido.springwebapplication.models.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
 public class productDao {
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf) {
+        this.sessionFactory = sf;
+    }
+
+    @Transactional
+    public List<Product> getProducts() {
+        return this.sessionFactory.getCurrentSession().createQuery("from PRODUCT").list();
+    }
+
+    @Transactional
+    public Product addProduct(Product product) {
+        this.sessionFactory.getCurrentSession().save(product);
+        return product;
+    }
+
+    @Transactional
+    public Product getProduct(int id) {
+        return this.sessionFactory.getCurrentSession().get(Product.class, id);
+    }
+
+    public Product updateProduct(Product product) {
+        this.sessionFactory.getCurrentSession().update(String.valueOf(Product.class), product);
+        return product;
+    }
+
+    @Transactional
+    public Boolean deletProduct(int id) {
+
+        Session session = this.sessionFactory.getCurrentSession();
+        Object persistanceInstance = session.load(Product.class, id);
+
+        if (persistanceInstance != null) {
+            session.delete(persistanceInstance);
+            return true;
+        }
+        return false;
+    }
 }
